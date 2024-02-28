1cb17b226b71e9490d0c11d57d4348c24f068859
Melvin Sanchez <melsava1207@gmail.com>
2024-02-27 21:37:38 -0600

    Creacion del componenete Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import { FaSignInAlt, FaUser } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='logo'>
+        <Link to = '/'>App de Tareas</Link>
+      </div>
+      <ul>
+        <li>
+          <Link to='/login'>
+            <FaSignInAlt/> Login
+          </Link>
+        </li>
+        <li>
+          <Link to='/register'>
+            <FaUser/> Registrar
+          </Link>
+        </li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header
